08bcd2035cbf33d0b98718f17124923da3115ac9
Suphachok <suphachok.k@kkumail.com>
2024-02-09 23:42:05 +0700

    Revert "Add UAT"
    This reverts commit 51c2eb9d2200af59f6693f4f814279d5389ac416.

---
+++ Project/var/cache/prod/smarty/cache/ps_newproducts/1/1/2/204/classic/45/10/de/4510deb5f45b4f7809253bf32e250142a6f088d6.ps_newproductsviewstemplateshookps_newproducts.tpl.php
@@ -0,0 +1,339 @@
+<?php
+/* Smarty version 4.3.1, created on 2024-01-30 19:06:10
+  from 'module:ps_newproductsviewstemplateshookps_newproducts.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_65b8e632978b42_58318966',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4a8df44403a47041b050fac755e17268c2a7c3e7' => 
+    array (
+      0 => 'module:ps_newproductsviewstemplateshookps_newproducts.tpl',
+      1 => 1706534575,
+      2 => 'module',
+    ),
+    'a4c42e0abed72790bc7a19ad254d14cab2f76a9e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\se_project\\Project\\themes\\classic\\templates\\catalog\\_partials\\productlist.tpl',
+      1 => 1706534575,
+      2 => 'file',
+    ),
+    'f3b52559e3aabe001a62276f5d1f5c2ce91bd7d2' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\se_project\\Project\\themes\\classic\\templates\\catalog\\_partials\\miniatures\\product.tpl',
+      1 => 1706534575,
+      2 => 'file',
+    ),
+    'ac8a5f7c06bce6fea5be4882c6ee940f8085a2f9' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\se_project\\Project\\themes\\classic\\templates\\catalog\\_partials\\product-flags.tpl',
+      1 => 1706534575,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_65b8e632978b42_58318966 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<section class="featured-products clearfix mt-3">
+  <h2 class="h2 products-section-title text-uppercase">
+    สินค้าใหม่
+  </h2>
+  
+
+<div class="products">
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="24" data-id-product-attribute="0">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/se_project/Project/th/hna-hlak/24-kku-shrit-aniversary-60-years.html" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/se_project/Project/31-home_default/kku-shrit-aniversary-60-years.jpg"
+                  alt="KKU Shrit aniversary 60 years"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/se_project/Project/31-large_default/kku-shrit-aniversary-60-years.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations no-variants">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> เปิดหน้าต่างย่อ
+            </a>
+          
+
+          
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/se_project/Project/th/hna-hlak/24-kku-shrit-aniversary-60-years.html" content="http://localhost/se_project/Project/th/hna-hlak/24-kku-shrit-aniversary-60-years.html">KKU Shrit aniversary 60 years</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="ราคา">
+                                                  ฿999.00
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="24" data-url="http://localhost/se_project/Project/th/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">ใหม่</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="23" data-id-product-attribute="0">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/se_project/Project/th/hna-hlak/23-kku-bag-canvas.html" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/se_project/Project/29-home_default/kku-bag-canvas.jpg"
+                  alt="KKU Bag canvas"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/se_project/Project/29-large_default/kku-bag-canvas.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations no-variants">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> เปิดหน้าต่างย่อ
+            </a>
+          
+
+          
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/se_project/Project/th/hna-hlak/23-kku-bag-canvas.html" content="http://localhost/se_project/Project/th/hna-hlak/23-kku-bag-canvas.html">KKU Bag canvas</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="ราคา">
+                                                  ฿250.00
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="23" data-url="http://localhost/se_project/Project/th/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">ใหม่</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="22" data-id-product-attribute="0">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/se_project/Project/th/hna-hlak/22-kku-souvenir.html" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/se_project/Project/30-home_default/kku-souvenir.jpg"
+                  alt="KKU Souvenir"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/se_project/Project/30-large_default/kku-souvenir.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations no-variants">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> เปิดหน้าต่างย่อ
+            </a>
+          
+
+          
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/se_project/Project/th/hna-hlak/22-kku-souvenir.html" content="http://localhost/se_project/Project/th/hna-hlak/22-kku-souvenir.html">KKU Souvenir</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="ราคา">
+                                                  ฿999.00
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="22" data-url="http://localhost/se_project/Project/th/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">ใหม่</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+            
+<div class="js-product product col-xs-12 col-sm-6 col-lg-4 col-xl-3">
+  <article class="product-miniature js-product-miniature" data-id-product="20" data-id-product-attribute="0">
+    <div class="thumbnail-container">
+      <div class="thumbnail-top">
+        
+                      <a href="http://localhost/se_project/Project/th/clothes/20-kku-t-shirt.html" class="thumbnail product-thumbnail">
+              <picture>
+                                                <img
+                  src="http://localhost/se_project/Project/28-home_default/kku-t-shirt.jpg"
+                  alt="KKU T-shirt"
+                  loading="lazy"
+                  data-full-size-image-url="http://localhost/se_project/Project/28-large_default/kku-t-shirt.jpg"
+                  width="250"
+                  height="250"
+                />
+              </picture>
+            </a>
+                  
+
+        <div class="highlighted-informations no-variants">
+          
+            <a class="quick-view js-quick-view" href="#" data-link-action="quickview">
+              <i class="material-icons search">&#xE8B6;</i> เปิดหน้าต่างย่อ
+            </a>
+          
+
+          
+                      
+        </div>
+      </div>
+
+      <div class="product-description">
+        
+                      <h3 class="h3 product-title"><a href="http://localhost/se_project/Project/th/clothes/20-kku-t-shirt.html" content="http://localhost/se_project/Project/th/clothes/20-kku-t-shirt.html">KKU T-shirt</a></h3>
+                  
+
+        
+                      <div class="product-price-and-shipping">
+              
+              
+
+              <span class="price" aria-label="ราคา">
+                                                  ฿369.00
+                              </span>
+
+              
+
+              
+            </div>
+                  
+
+        
+          
+<div class="product-list-reviews" data-id="20" data-url="http://localhost/se_project/Project/th/module/productcomments/CommentGrade">
+  <div class="grade-stars small-stars"></div>
+  <div class="comments-nb"></div>
+</div>
+
+        
+      </div>
+
+      
+    <ul class="product-flags js-product-flags">
+                    <li class="product-flag new">ใหม่</li>
+            </ul>
+
+    </div>
+  </article>
+</div>
+
+    </div>
+  <a class="all-product-link float-xs-left float-md-right h4" href="http://localhost/se_project/Project/th/สินค้าใหม่">
+    ดูสินค้าใหม่ทั้งหมด<i class="material-icons">&#xE315;</i>
+  </a>
+</section>
+
+<?php }
+}
