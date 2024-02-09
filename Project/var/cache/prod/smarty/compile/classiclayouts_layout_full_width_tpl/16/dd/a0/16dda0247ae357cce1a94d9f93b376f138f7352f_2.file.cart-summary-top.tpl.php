5043a754a2395645db2af6f6767609caebfb1b35
Suphachok <suphachok.k@kkumail.com>
2024-02-09 23:41:57 +0700

    Revert "Revert "Merge pull request #28 from KanlayaniS/master""
    This reverts commit b2a63d0fea37bf99a2bf7da7f38bdca3fb69ec8e.

---
+++ Project/var/cache/prod/smarty/compile/classiclayouts_layout_full_width_tpl/16/dd/a0/16dda0247ae357cce1a94d9f93b376f138f7352f_2.file.cart-summary-top.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/* Smarty version 4.3.1, created on 2024-01-30 19:38:35
+  from 'C:\xampp\htdocs\se_project\Project\themes\classic\templates\checkout\_partials\cart-summary-top.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_65b8edcbed3a06_13964394',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '16dda0247ae357cce1a94d9f93b376f138f7352f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\se_project\\Project\\themes\\classic\\templates\\checkout\\_partials\\cart-summary-top.tpl',
+      1 => 1706617826,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_65b8edcbed3a06_13964394 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="cart-summary-top js-cart-summary-top">
+  <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>'displayCheckoutSummaryTop'),$_smarty_tpl ) );?>
+
+</div>
+<?php }
+}
